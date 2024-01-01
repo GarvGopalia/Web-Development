480b123f7148f631017cfbc9da4df48af1705fe3
GarvGopalia <garvgopalia@gmail.com>
2024-01-01 12:35:49 +0530

    done

---
+++ JS/Part 2/app.js
@@ -0,0 +1,11 @@
+// console.log("Hello World!");
+// console.log("Deprived");
+let a = 5;
+let b = 8;
+console.log("sum is : ", a + b);
+
+let pencilPrice =  10;
+let erasorPrice = 5;
+// let output = "The total price is : " + (pencilPrice + erasorPrice) + "Rupees.";
+
+console.log(`The total price is : ${pencilPrice+erasorPrice}`);
