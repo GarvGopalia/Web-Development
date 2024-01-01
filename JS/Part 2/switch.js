8a7f35e33b25e6a20730fb87069b53ed510b31b5
GarvGopalia <garvgopalia@gmail.com>
2024-01-01 17:08:09 +0530

    done

---
+++ JS/Part 2/switch.js
@@ -0,0 +1,15 @@
+let color = "red";
+
+switch(color) {
+    case "red" :
+        console.log ("stop");
+    break;
+    case "yellow" :
+        console.log ("slow down");
+    break;
+    case "green" :
+        console.log ("Go");
+    break;
+    default :
+        console.log("Broken Lights")
+}
