41e2c14315ed307764c6726ecb355256706f2be6
GarvGopalia <92243018+GarvGopalia@users.noreply.github.com>
2024-01-04 23:46:39 +0530

    Add files via upload

---
+++ JS/Part 4/favmovie.js
@@ -0,0 +1,13 @@
+const fav = "Dark";
+let guess = prompt("Enter your guess :");
+
+while((guess != fav) && (guess !="quite")){
+    guess = prompt("Re-enter you guess :")
+}
+
+if(guess == fav) {
+    console.log("Congrulation you have guessed the right!!")
+} else if(guess == "quite"){
+    console.log("You quite the game");
+}
+
