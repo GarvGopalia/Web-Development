bf547eb9ffde57e7976534a96985cfc86411539a
GarvGopalia <92243018+GarvGopalia@users.noreply.github.com>
2024-01-08 15:42:35 +0530

    Add files via upload

---
+++ JS/Part 5/assignmentQ.js
@@ -0,0 +1,22 @@
+//Question 2
+const cars = {
+    name: "Range Rover",
+    model: "Discovery",
+    color: ["red","black"]
+};
+console.log(cars.name);
+
+//Question 3
+const person = {
+    name: "Garv Gopalia",
+    age: 20,
+    city: "Las Vegas"
+};
+person.city = "New York";
+person.country = "United States";
+
+console.log(person);
+
+//Question 1
+const random = Math.floor(Math.random() * 6) + 1;
+console.log(random);
---
+++ JS/Part 5/game.js
@@ -0,0 +1,22 @@
+const max = prompt("Enter the max number:");
+
+const random = Math.floor(Math.random() * max) + 1;
+console.log(random);
+
+let guess = prompt("guess the number:");
+
+while(true) {
+    if(guess == "quit") {
+        console.log("user quite");
+        break;
+    }
+
+    if(guess == random) {
+        console.log("you are right! congrats");
+        break;
+    } else if(guess < random) {
+        guess = prompt("hint : your guess was too small. Please try again")
+    } else {
+        guess = prompt("hint : your guess was too large. please try again")
+    }
+}
