801474c07306786b33a898edc51372b0a2145607
GarvGopalia <garvgopalia@gmail.com>
2024-01-11 11:07:48 +0530

    done

---
+++ JS/Part 8/destructuring.js
@@ -0,0 +1,6 @@
+let names = ["tony","bruce","peter","steve"];
+// let winner = names[0];
+// let runnerup = names[1];
+// let secondrunnerup = names[2];
+
+let [winner, runnerup, ...others] = names;
