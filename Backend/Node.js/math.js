a4426eeb9f976581411e6830bd649a889fbe2af5
GarvGopalia <garvgopalia@gmail.com>
2024-01-22 19:24:43 +0530

    done

---
+++ Backend/Node.js/math.js
@@ -0,0 +1,20 @@
+// module.exports.sum = (a, b) => a+b;
+const mul = (a, b) => a*b;
+const g = 9.8;
+const PI = 3.14;
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI,
+// };
+
+// module.exports = obj;
+
+module.exports = {
+    // sum: sum,
+    mul: mul,
+    g: g,
+    PI: PI,
+};
