ac99ad66b3e48bc8d51d21548aa70da29011f1ea
GarvGopalia <92243018+GarvGopalia@users.noreply.github.com>
2024-01-31 23:17:53 +0530

    Add files via upload

---
+++ Backend/REST/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const {v4: uuidv4 } = require("uuid");
+uuidv4();
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+var posts = [
+    {   id: uuidv4(),
+        username: "me__garv",
+        content: "i love coding",
+    },
+    {
+        id: uuidv4(),
+        username: "garvgopalia",
+        content: "i playing chess",
+    },
+    {
+        id: uuidv4(),
+        username: "garv__privv",
+        content: "this is my private account"
+    }
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts})
+})
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs")
+});
+
+app.post("/posts", (req, res) => {
+    let {username, content}= req.body;
+    let id = uuidv4();
+    posts.push({id, username, content});
+    res.redirect("/posts")
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", { post })
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    res.send("patch request working");
+})
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);   
+});
